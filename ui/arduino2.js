2846bdd16507376923e64b68793c9e720fad77c4
abhay2132 <abhaybro768@gmail.com>
2024-03-13 15:01:11 +0530

    all done in simulation

--- ui/arduino2.js
+++ ui/arduino2.js
@@ -1,70 +1,83 @@
 // import { goalReached } from "./botUtils.js";
 import { botData, mazeData } from "./data.js";
-import { FloodFill, emptyFloodMatrix , getNLocs, isWallin} from "./floodFill.js";
+import { FloodFill, emptyFloodMatrix, getNLocs, isWallin } from "./floodFill.js";
 import { updateUI } from "./maze.js";
-import { generateEmptyWallMatrix, updateKnownWallsUI, updateFloodUI } from "./mazeUtils.js";
-import { argsMin } from "./utils.js";
-import { stepForward, checkFront, checkLeft, checkRight, moveTo , turnLeft, turnRight, getFront, getRight, getLeft, goalReached, updateBotDir, uTurn } from "./botUtils.js";
+import { generateEmptyWallMatrix, updateKnownWallsUI, updateFloodUI, swapGS } from "./mazeUtils.js";
+import { argsMin, wait } from "./utils.js";
+import { stepForward, checkFront, checkLeft, checkRight, moveTo, turnLeft, turnRight, getFront, getRight, getLeft, goalReached, updateBotDir, uTurn } from "./botUtils.js";
 import { $ } from "./utils.js";
 
 export var walls;
-export var floodMatrix ;
-var stage = 1; // 1: Start -> Goal  | 2: Wall encountered
+export var floodMatrix;
+var stage = 1; // 1: Start -> Goal  | 2: Wall encountered | 3: Goal Reached | 4 : SpeedRun | 5 : FINISHED
 var path = [];
 var bestPath = [];
 
-function addWalls(){
-    let [x,y] = botData.pos;
-    let {size} = mazeData;
-    switch(botData.dir){
+const maxCycles = 2;
+var runType = 1;// 1 : start -> Goal , 2 : goal -> start
+var currCycle = 1;
+var speedRunDone = false;
+
+function addWalls() {
+    let [x, y] = botData.pos;
+    let { size } = mazeData;
+    switch (botData.dir) {
         case 1: // bot facing north
-            if(checkFront() && y > 0) walls[y-1][1][x] = 1;
-            if(checkLeft() && x > 0) walls[y][0][x-1] = 1;
-            if(checkRight() && x < size -1 ) walls[y][0][x] = 1;
+            if (checkFront() && y > 0) walls[y - 1][1][x] = 1;
+            if (checkLeft() && x > 0) walls[y][0][x - 1] = 1;
+            if (checkRight() && x < size - 1) walls[y][0][x] = 1;
             break;
         case 2: // bot facing east
-            if(checkFront() && x < size-1) walls[y][0][x] = 1;
-            if(checkLeft() && y > 0) walls[y-1][1][x] = 1;
-            if(checkRight() && y < size -1 ) walls[y][1][x] = 1;
+            if (checkFront() && x < size - 1) walls[y][0][x] = 1;
+            if (checkLeft() && y > 0) walls[y - 1][1][x] = 1;
+            if (checkRight() && y < size - 1) walls[y][1][x] = 1;
             break;
         case 3: // bot facing south
-            if(checkFront() && y < size-1) walls[y][1][x] = 1;
-            if(checkLeft() && x < size-1) walls[y][0][x] = 1;
-            if(checkRight() && x >0 ) walls[y][0][x-1] = 1;
+            if (checkFront() && y < size - 1) walls[y][1][x] = 1;
+            if (checkLeft() && x < size - 1) walls[y][0][x] = 1;
+            if (checkRight() && x > 0) walls[y][0][x - 1] = 1;
             break;
         case 4: // bot facing west
-            if(checkFront() && x > 0) walls[y][0][x-1] = 1;
-            if(checkLeft() && y < size-1) walls[y][1][x] = 1;
-            if(checkRight() && y > 0 ) walls[y-1][1][x] = 1;
-            break;   
+            if (checkFront() && x > 0) walls[y][0][x - 1] = 1;
+            if (checkLeft() && y < size - 1) walls[y][1][x] = 1;
+            if (checkRight() && y > 0) walls[y - 1][1][x] = 1;
+            break;
     }
 
     updateKnownWallsUI(walls);
 }
 
-export function setup(){
+export function setup() {
     walls = generateEmptyWallMatrix(mazeData.size)
     floodMatrix = emptyFloodMatrix(mazeData.size);
     FloodFill(floodMatrix, [mazeData.goal], walls);
 }
 
-export async function loop(){
-    if(floodMatrix.length != mazeData.size) setup(); // when maze updated, only in simulation
-    let [x,y] = botData.pos;
-    if(stage == 1){
-        
+export async function loop() {
+    if (floodMatrix.length != mazeData.size) setup(); // when maze updated, only in simulation
+    let [x, y] = botData.pos;
+    if (stage == 1) {
+
+
+        if (path.length > 1 && path[path.length - 2] == x + "-" + y) {
+            path.pop();
+        } else {
+            path.push(x + "-" + y);
+        }
         // add new walls
         addWalls();
-        if(goalReached()){
+
+        // check if goal reached
+        if (goalReached()) {
             stage = 3;
             return;
         }
-        
+
 
         // min distance neighbours
-        let n = getNLocs(mazeData.size, [x,y]);
+        let n = getNLocs(mazeData.size, [x, y]);
         let minIs = argsMin(n.map(_ => Array.isArray(_) ? floodMatrix[_[1]][_[0]] : Infinity));
-        
+
         // while(minIs.length > 0){
         //     let target = n[minIs.shift()];
         //     if(!isWallin([x,y], target,walls)){
@@ -72,35 +85,96 @@ export async function loop(){
         //         return;
         //     }
         // }
-        n = n.filter(Boolean).sort(([ax,ay],[bx,by]) => (floodMatrix[ay][ax] - floodMatrix[by][bx]))
-        .filter(([nx,ny])=>floodMatrix[ny][nx] < floodMatrix[y][x])
-        while(n.length > 0){
+        n = n.filter(Boolean).sort(([ax, ay], [bx, by]) => (floodMatrix[ay][ax] - floodMatrix[by][bx]))
+            .filter(([nx, ny]) => floodMatrix[ny][nx] < floodMatrix[y][x])
+        while (n.length > 0) {
             let target = n.shift();
-            if(!isWallin([x,y], target, walls)){
+            if (!isWallin([x, y], target, walls)) {
                 moveTo(target);
                 return;
             }
         }
 
         // all min. neighbours are not accessable
-        stage=2;
+        stage = 2;
         return;
     }
 
-    if(stage == 2){
+    if (stage == 2) {
         FloodFill(floodMatrix, [mazeData.goal], walls);
         updateFloodUI(floodMatrix);
         stage = 1;
-        return ;
+        return;
     }
+
+    if (stage == 3) { // when we reached from either goal to start or start to goal
+
+        if(bestPath.length == 0){
+            for(let i=0; i< path.length ; i++){
+                bestPath.push(path[i]);
+            }
+        }
+        else if(path.length < bestPath.length){
+            bestPath.length = 0;
+            for(let i=0; i< path.length ; i++){
+                bestPath.push(path[i]);
+            }
+        }
+        path.length = 0;
+        // if(currCycle <= maxCycles){
+        if (runType == 1) { // go back to start
+            // swap start <--> goal
+            swapGS();
+            runType = 2;
+            stage = 1;
+            FloodFill(floodMatrix, [mazeData.goal], walls);
+        }
+        else if (runType == 2) {
+            if (currCycle == maxCycles) {
+                stage = 4;
+                return;
+            }
+            currCycle += 1;
+            swapGS();
+            runType = 1;
+            stage = 1;
+            FloodFill(floodMatrix, [mazeData.goal], walls);
+        }
+        return;
+    }
+
+    if (stage == 4) {
+        let [x,y] = botData.pos;
+
+        let [sx,sy] = bestPath[0].split("-").map(n=>parseInt(n));
+        let [ex,ey] = bestPath[bestPath.length - 1].split("-").map(n=>parseInt(n));
+
+        if(sx==x && sy==y){
+            for(let i=0; i<bestPath.length ; i++){
+                moveTo(bestPath[i].split("-").map(n=>parseInt(n)));
+                await wait(100);
+            }
+        }
+        else if(ex==x && ey==y){
+            for(let i=bestPath.length-1; i>=0; i--){
+                await wait(100);
+                moveTo(bestPath[i].split("-").map(n=>parseInt(n)));
+            }
+        }
+
+        stage = 5;
+        return;
+    }
+
+
 }
 
-export function reset(){
+export function reset() {
     // setup();
     stage = 1;
     path.length = 0;
     botData.pos = Array.from(mazeData.start);
-    path.push([botData.pos]);
+    // path.push([botData.pos]);
 
     // FloodFill(floodMatrix, [mazeData.goal], walls);
 
@@ -108,11 +182,11 @@ export function reset(){
     resetFloodUI();
 }
 
-function resetFloodUI(){
-    for(let y=0; y<floodMatrix.length; y++){
-        for(let x=0; x<floodMatrix[y].length; x++){
+function resetFloodUI() {
+    for (let y = 0; y < floodMatrix.length; y++) {
+        for (let x = 0; x < floodMatrix[y].length; x++) {
             let val = floodMatrix[y][x];
-            $("#c"+y+"-"+x).innerHTML = (val == Infinity ? 'i' : val);
+            $("#c" + y + "-" + x).innerHTML = (val == Infinity ? 'i' : val);
         }
     }
 }
--- ui/mazeUtils.js
+++ ui/mazeUtils.js
@@ -2,48 +2,48 @@ import { botData, mazeData } from "./data.js";
 import { FloodFill, emptyFloodMatrix } from "./floodFill.js";
 import { $, $$ } from "./utils.js";
 
-const _maze = (mazeBody="", mazeId="", size=0) =>  `<div id="${mazeId}" data-size="${size}" class="maze">${mazeBody}</div>`;
-const _row = (rowBody="", _id="") => ` <div class="row" id=${_id} >${rowBody}</div>`;
+const _maze = (mazeBody = "", mazeId = "", size = 0) => `<div id="${mazeId}" data-size="${size}" class="maze">${mazeBody}</div>`;
+const _row = (rowBody = "", _id = "") => ` <div class="row" id=${_id} >${rowBody}</div>`;
 
-const _v_row = (b="", _id="") => `<div class="v-wall-row" id=${_id} >${b}</div>`;
-const _h_row = (b="", _id="") => `<div class="h-wall-row" id=${_id} >${b}</div>`;
+const _v_row = (b = "", _id = "") => `<div class="v-wall-row" id=${_id} >${b}</div>`;
+const _h_row = (b = "", _id = "") => `<div class="h-wall-row" id=${_id} >${b}</div>`;
 
-const _cell = (n="", _id="") => ` <div class="cell" id=${_id} >${n}</div>`
+const _cell = (n = "", _id = "") => ` <div class="cell" id=${_id} >${n}</div>`
 
-const _v_wall = (_id="", mode="off") => `<div class="v-wall wall" type=unknown id=${_id} mode=${mode}></div>`;
-const _h_wall = (_id="", mode="off") => `<div class="h-wall wall" type=unknown id=${_id} mode=${mode}></div>`;
+const _v_wall = (_id = "", mode = "off") => `<div class="v-wall wall" type=unknown id=${_id} mode=${mode}></div>`;
+const _h_wall = (_id = "", mode = "off") => `<div class="h-wall wall" type=unknown id=${_id} mode=${mode}></div>`;
 
-const _wall_col = ( _id="") => `<div class="wall-col wall" id=${_id} ></div>`;
+const _wall_col = (_id = "") => `<div class="wall-col wall" id=${_id} ></div>`;
 
-const cid = (x,y) => "c"+x+"-"+y;
-const vid = (x,y) => "v"+x+"-"+y;
-const hid = (x,y) => "h"+x+"-"+y;
-const iid = (x,y) => "i"+x+"-"+y;
+const cid = (x, y) => "c" + x + "-" + y;
+const vid = (x, y) => "v" + x + "-" + y;
+const hid = (x, y) => "h" + x + "-" + y;
+const iid = (x, y) => "i" + x + "-" + y;
 
-export function makeMaze(size=0, _id){
+export function makeMaze(size = 0, _id) {
     let rows = "";
-    for(let y=0 ; y<size; y++){
+    for (let y = 0; y < size; y++) {
         let row = "";
         let v_row = "";
         let h_row = "";
 
         // For Vertical Wall Column
-        for(let x=0; x<size*2-1; x++){
-            let _x = parseInt(x/2);
-            let item = (x%2==0) ? _cell(y+","+_x,cid(y,_x)) : _v_wall(vid(y,_x));
+        for (let x = 0; x < size * 2 - 1; x++) {
+            let _x = parseInt(x / 2);
+            let item = (x % 2 == 0) ? _cell(y + "," + _x, cid(y, _x)) : _v_wall(vid(y, _x));
             v_row += item;
         }
 
         // For Horizontal Wall Column
-        if(y<size-1){
-            for(let x=0; x<size*2-1;x++){
-                let _x = parseInt(x/2);
-                let item = (x%2==0) ? _h_wall(hid(y,_x)):_wall_col(iid(y,_x));
+        if (y < size - 1) {
+            for (let x = 0; x < size * 2 - 1; x++) {
+                let _x = parseInt(x / 2);
+                let item = (x % 2 == 0) ? _h_wall(hid(y, _x)) : _wall_col(iid(y, _x));
                 h_row += item;
             }
         }
 
-        row = _v_row(v_row, y+"-0") + _h_row(h_row, y+"-1");
+        row = _v_row(v_row, y + "-0") + _h_row(h_row, y + "-1");
         rows += _row(row, y);
     }
 
@@ -51,49 +51,49 @@ export function makeMaze(size=0, _id){
 }
 
 
-export const getCell = (mazeId, x,y) => {
+export const getCell = (mazeId, x, y) => {
     return $(`#${mazeId} #c${y}-${x}`)
 }
 
-export const setCellColor = (mazeId, x,y, bgcolor, color) => {
-    getCell(mazeId, x,y).style.setProperty("color", color);
-    getCell(mazeId, x,y).style.setProperty("background-color", bgcolor);
+export const setCellColor = (mazeId, x, y, bgcolor, color) => {
+    getCell(mazeId, x, y).style.setProperty("color", color);
+    getCell(mazeId, x, y).style.setProperty("background-color", bgcolor);
 }
 
 export const getWallMatrix = (mazeId) => {
     const wallMatrix = [];
 
-    const size = parseInt($("#"+mazeId).dataset.size);
+    const size = parseInt($("#" + mazeId).dataset.size);
 
-    for(let y=1; y <= size ; y++){
+    for (let y = 1; y <= size; y++) {
         let v_walls = $$(`#${mazeId} > .row:nth-child(${y}) > .v-wall-row > .wall`).map(t => t.getAttribute("mode") === "on" ? 1 : 0);
         let arr = [v_walls];
 
-        if(y < size){
-            let h_walls = $$(`#${mazeId} > .row:nth-child(${y}) > .h-wall-row > .wall`).filter(t=>!t.classList.contains("wall-col")).map(t => t.getAttribute("mode") === "on" ? 1 : 0);
+        if (y < size) {
+            let h_walls = $$(`#${mazeId} > .row:nth-child(${y}) > .h-wall-row > .wall`).filter(t => !t.classList.contains("wall-col")).map(t => t.getAttribute("mode") === "on" ? 1 : 0);
             arr.push(h_walls);
         }
 
         wallMatrix.push(arr)
     }
-    
+
     return wallMatrix;
 }
 
-export function loadMaze(mazeData){
-    let {mazeId, wallMatrix, size, goal, start}=mazeData;
+export function loadMaze(mazeData) {
+    let { mazeId, wallMatrix, size, goal, start } = mazeData;
 
     $("#maze-body").innerHTML = makeMaze(size, mazeId)
-    $("#maze-title").innerHTML = size+"x"+size;
+    $("#maze-title").innerHTML = size + "x" + size;
     $("#maze-size").value = size;
-    for(let y=0 ;y<wallMatrix.length && y < size; y++){
-        let [v_walls , h_walls] = wallMatrix[y];
+    for (let y = 0; y < wallMatrix.length && y < size; y++) {
+        let [v_walls, h_walls] = wallMatrix[y];
 
-        v_walls.forEach((mode,x) => {
-            $(`#${mazeId} .wall#v${y}-${x}`).setAttribute("mode", mode ? "on":"off");
-        })   
-        h_walls && h_walls.forEach((mode,x)=>{
-            $(`#${mazeId} .wall#h${y}-${x}`).setAttribute("mode", mode ? "on":"off");
+        v_walls.forEach((mode, x) => {
+            $(`#${mazeId} .wall#v${y}-${x}`).setAttribute("mode", mode ? "on" : "off");
+        })
+        h_walls && h_walls.forEach((mode, x) => {
+            $(`#${mazeId} .wall#h${y}-${x}`).setAttribute("mode", mode ? "on" : "off");
         })
     }
 
@@ -106,88 +106,88 @@ export function loadMaze(mazeData){
     return 1;
 }
 
-export function parseID(tag){
+export function parseID(tag) {
     let id = tag.id.split("-");
     let x = id[1];
     let y = id[0].slice(1);
-    return [parseInt(x),parseInt(y)]
+    return [parseInt(x), parseInt(y)]
 }
 
-const getMazeSize = mazeId => parseInt($("#"+mazeId).dataset.size);
+const getMazeSize = mazeId => parseInt($("#" + mazeId).dataset.size);
 
-export function getCellMatrix(mazeId){
+export function getCellMatrix(mazeId) {
     let size = getMazeSize(mazeId);
     let cellMatrix = [];
     let cells = $$(".cell");
     let tmp = [];
-    
-    for(let i=1; i<=size*size; i++){
-        tmp.push(cells[i-1]);
-        if(i % size == 0) {
+
+    for (let i = 1; i <= size * size; i++) {
+        tmp.push(cells[i - 1]);
+        if (i % size == 0) {
             cellMatrix.push(tmp);
-            tmp= Array();
+            tmp = Array();
         }
     }
     return cellMatrix;
 }
 
-export function generateEmptyWallMatrix (size){
-    let wm = Array.from({length:size-1}).map(e => [
-        Array.from({length: size-1}).map(_=> 0),
-        Array.from({length: size}).map(_=>0)
+export function generateEmptyWallMatrix(size) {
+    let wm = Array.from({ length: size - 1 }).map(e => [
+        Array.from({ length: size - 1 }).map(_ => 0),
+        Array.from({ length: size }).map(_ => 0)
     ]);
-    wm.push([Array.from({length:size-1}).map(_=>0)]);
+    wm.push([Array.from({ length: size - 1 }).map(_ => 0)]);
     return wm;
 }
 
-export function updateBotLocUI(){
+export function updateBotLocUI() {
     $(".active").classList.remove("active");
     $(`#c${botData.pos[1]}-${botData.pos[0]}`).classList.add("active");
 }
 
-export function getDistMatrix(){
-    let [gx,gy] = mazeData.goal;
+export function getDistMatrix() {
+    let [gx, gy] = mazeData.goal;
     let dm = [];
-    for(let y=0; y<mazeData.size ; y++){
+    for (let y = 0; y < mazeData.size; y++) {
         dm.push(
-            Array.from({length:mazeData.size}).map((e,x)=>(Math.abs(y-gy)+Math.abs(x-gx)))
+            Array.from({ length: mazeData.size }).map((e, x) => (Math.abs(y - gy) + Math.abs(x - gx)))
         )
     }
     return dm;
 }
 
-export function updateGoalUI(){
+export function updateGoalUI() {
     let [gx, gy] = mazeData.goal;
-    
+
     $(".goal").classList.remove("goal");
-    $("#c"+gy+"-"+gx).classList.add("goal");
+    $("#c" + gy + "-" + gx).classList.add("goal");
 }
 
-export function updateStartUI(){
+export function updateStartUI() {
     let [sx, sy] = mazeData.start;
     // $("label[for=cell-dist]").click();
-    
+
     $(".start").classList.remove("start");
-    $("#c"+sy+"-"+sx).classList.add("start");
+    $("#c" + sy + "-" + sx).classList.add("start");
 }
 
-export function updateActiveUI(){
+export function updateActiveUI() {
     let [x, y] = botData.pos;
-    
+
     $(".active").classList.remove("active");
-    $("#c"+y+"-"+x).classList.add("active");
+    $("#c" + y + "-" + x).classList.add("active");
 }
 
-export function updateKnownWallsUI(walls){
-    for(let y=0; y<walls.length; y++){
+export function updateKnownWallsUI(walls) {
+    for (let y = 0; y < walls.length; y++) {
         let [vWalls, hWalls] = walls[y];
-        for(let x=0; x<vWalls.length; x++){
-            $("#v"+y+"-"+x).setAttribute("type", vWalls[x]==1 ? "known":"unknown");
+        for (let x = 0; x < vWalls.length; x++) {
+            $("#v" + y + "-" + x).setAttribute("type", vWalls[x] == 1 ? "known" : "unknown");
         }
-        if(hWalls && hWalls.length > 0)
-        for(let x=0; x<hWalls.length; x++){
-            $("#h"+y+"-"+x).setAttribute("type", (hWalls[x])==1 ? "known":"unknown");
-        }
+        if (hWalls && hWalls.length > 0)
+            for (let x = 0; x < hWalls.length; x++) {
+                $("#h" + y + "-" + x).setAttribute("type", (hWalls[x]) == 1 ? "known" : "unknown");
+            }
     }
 
     console.log(walls);
@@ -197,11 +197,17 @@ export function updateKnownWallsUI(walls){
  * Updates the Maze cell values acc. to flood Matrix
  * @param {[[int]]} floodMatrix 
  */
-export function updateFloodUI(floodMatrix){
+export function updateFloodUI(floodMatrix) {
     let size = floodMatrix.length;
-    for(let y=0; y<size ; y++){
-        for(let x=0; x<size; x++){
-            $("#c"+y+"-"+x).innerHTML = floodMatrix[y][x];
+    for (let y = 0; y < size; y++) {
+        for (let x = 0; x < size; x++) {
+            $("#c" + y + "-" + x).innerHTML = floodMatrix[y][x];
         }
     }
+}
+
+export function swapGS() {
+    let g1 = mazeData.goal;
+    mazeData.goal = Array.from(mazeData.start);
+    mazeData.start = Array.from(g1);
 }
